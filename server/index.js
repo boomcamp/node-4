c9c53d905408b7e179fc9297a312846085fca97a
HamletDelosReyes <hamlet.delosreyes@boom.camp>
2020-01-07 15:08:34 +0800

    Submission

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const massive = require("massive");
+
+const users = require("../controllers/users");
+
+massive({
+  host: "localhost",
+  port: 5432,
+  database: "node4db",
+  user: "postgres",
+  password: "node4db"
+})
+  .then(db => {
+    const app = express();
+
+    app.set("db", db);
+
+    app.use(express.json());
+
+    app.post("/api/register", users.register);
+
+    app.get("/api/protected/data", users.authorize);
+
+    app.post("/api/login", users.login);
+    const PORT = 5004;
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch(console.error);
