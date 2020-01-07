55b4ea5450b6941bf0d269e98c2310503eaa90b4
rapraquion <rapraquion@gmail.com>
2020-01-07 11:18:31 +0800

    Parts 1-2 done, working on Part 3

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const massive = require('massive');
+
+const user = require('./controllers/user');
+const login = require('./controllers/login');
+
+massive({
+    host: 'localhost',
+    port: 5432,
+    database: 'node4db',
+    user: 'postgres',
+    password: 'node4db'
+})
+    .then(db => {
+        const app = express();
+        app.set('db', db);
+        app.use(express.json());
+
+        // New User
+        app.post('/api/register', user.register);
+
+        // Authorization
+        app.get('/api/protected/data', function (req, res) {
+            if (!req.headers.authorization) {
+                return res.status(401).end();
+            }
+
+            try {
+                const token = req.headers.authorization.split(' ')[1];
+                jwt.verify(token, secret);
+                res.status(200).json({ data: 'protected data' });
+            } catch (err) {
+                console.error(err);
+                res.status(401).end();
+            }
+        });
+
+        // Login user
+        app.post('/api/login', login.login)
+
+        const port = 3000;
+        app.listen('port', () => {
+            console.log(`Listening on port ${port}`);
+        });
+    })
